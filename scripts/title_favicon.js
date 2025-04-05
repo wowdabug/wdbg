6d8b48f075909d556ff58434246e480c9565c2df
WowDaBug <110950607+WowDaBug@users.noreply.github.com>
2025-04-04 20:25:57 -0500

    main pages

--- scripts/title_favicon.js
+++ scripts/title_favicon.js
@@ -3,13 +3,11 @@
     const defaultFavicon = "https://wowdabug.github.io/wdbg/images/favicon-32x32.png";
     const title = localStorage.getItem('customTitle');
     const favicon = localStorage.getItem('customFavicon');
-
     if (title) {
         document.title = title;
     } else {
         document.title = defaultTitle;
     }
-
     if (favicon) {
         let link = document.querySelector("link[rel~='icon']");
         if (!link) {
