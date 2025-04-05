a534ad8cff33e3e70cfe8ad771741c21e9e63f4f
WowDaBug <110950607+WowDaBug@users.noreply.github.com>
2025-04-05 08:45:06 -0500

    test something

--- scripts/title_favicon.js
+++ scripts/title_favicon.js
@@ -1,26 +1,19 @@
+<script>
 (function applyCustomSettings() {
-    const defaultFavicon = "https://wowdabug.github.io/wdbg/images/favicon-32x32.png";
     const title = localStorage.getItem('customTitle');
     const favicon = localStorage.getItem('customFavicon');
+
     if (title) {
         document.title = title;
     }
-    if (favicon) {
-        let link = document.querySelector("link[rel~='icon']");
-        if (!link) {
-            link = document.createElement('link');
-            link.rel = 'icon';
-            document.head.appendChild(link);
-        }
-        link.href = favicon;
-    } else {
-        let link = document.querySelector("link[rel~='icon']");
-        if (!link) {
-            link = document.createElement('link');
-            link.rel = 'icon';
-            document.head.appendChild(link);
-        }
-        link.href = defaultFavicon;
-    }
-})();
 
+    let link = document.querySelector("link[rel~='icon']");
+    if (!link) {
+        link = document.createElement('link');
+        link.rel = 'icon';
+        document.head.appendChild(link);
+    }
+
+    link.href = favicon || '../images/favicon-32x32.png';
+})();
+</script>
