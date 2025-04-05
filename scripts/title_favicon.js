156579f0c5f2acecb24feab83bd3e1bd9ba96420
WowDaBug <110950607+WowDaBug@users.noreply.github.com>
2025-04-04 20:13:31 -0500

    default fav t

--- scripts/title_favicon.js
+++ scripts/title_favicon.js
@@ -1,8 +1,14 @@
 (function applyCustomSettings() {
+    const defaultTitle = "WDBG";
+    const defaultFavicon = "https://wowdabug.github.io/wdbg/images/favicon-32x32.png";
     const title = localStorage.getItem('customTitle');
     const favicon = localStorage.getItem('customFavicon');
 
-    if (title) document.title = title;
+    if (title) {
+        document.title = title;
+    } else {
+        document.title = defaultTitle;
+    }
 
     if (favicon) {
         let link = document.querySelector("link[rel~='icon']");
@@ -12,5 +18,14 @@
             document.head.appendChild(link);
         }
         link.href = favicon;
+    } else {
+        let link = document.querySelector("link[rel~='icon']");
+        if (!link) {
+            link = document.createElement('link');
+            link.rel = 'icon';
+            document.head.appendChild(link);
+        }
+        link.href = defaultFavicon;
     }
 })();
+
